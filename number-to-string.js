7c83b720df8feb583e5694c6e204b4b2d05f0f3b
Mark Macaraig <mjustinmacaraig@gmail.com>
2021-10-06 03:49:09 +0000

    Initial commit with all my work so far

---
+++ number-to-string.js
@@ -0,0 +1,7 @@
+let n = 128;
+n = n.toString();
+console.log(n);
+
+/. official solution ./
+const n = 128
+    console.log(n.toString())
---
+++ rounding-numbers.js
@@ -0,0 +1,8 @@
+var roundUp = Math.round(1.5);
+var rounded = 'roundUp';
+console.log(roundUp);
+
+/. official solution ./
+const roundUp = 1.5
+const rounded = Math.round(roundUp)
+console.log(rounded)
